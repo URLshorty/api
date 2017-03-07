1287ba85210564f1c4385b92b3d8321ca3052813
NoneOfMaster <tepper.russell@gmail.com>
2017-03-07 10:04:28 -0500

    Furthering initial setup

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
 
-app.set('views', './client/views/')
-app.set('view engine', 'pug')
+// config bodyParser() for gathering POST data
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
-  res.render('index')
+// port & router
+const port = process.env.PORT || 3000
+const router = express.Router()
+
+router.get('/', function(req, res) {
+    res.json({ message: 'api hit' })   
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.use('/api', router)
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
 })
