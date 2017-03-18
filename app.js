e473da3b7da6716aa500c1598b1376dbfdd6dd15
NoneOfMaster <tepper.russell@gmail.com>
2017-03-18 17:02:06 -0400

    API endpoints setup

--- app.js
+++ app.js
@@ -30,36 +30,90 @@ app.use(bodyParser.json())
 const port = process.env.PORT || 3000
 
 // do logger and cookieParser()?
-  async function t() {
-
-        let test = await Url
-                    .query()
-                    .limit(10)
-                    .then(function (x) {
-                        console.log(x)
-                        return x
-                    })
-                    .catch(function (err) {
-                      console.log(err);
-                    });
-  }
-
-  t()
 
 router.get('/', function (req, res) {
-  res.send('check check 1 2')
+  res.send('check check 1 2, this is root path of the api, where the docs will go')
 })
 
-router.get('/topvisitedurls', function (req, res) {
-  res.send('check check 1 2')
+// TODO - format errors
+router.post('/urls', async function (req, res) {
+  // signed in can use User.createUrl()
+  // not signed in can use Url.create()
+  let newUrl =
+    await
+      Url
+        .create(req.query.address)
+        .then( (d) => d )
+        .catch( (er) => er )
+  res.send(
+    newUrl
+  )
 })
 
-// router.get('/topvisitedurls', getTopVisitedURLs)
-// router.get('/toprequestedurls', getTopRequestedURLs)
-// router.post('/urls', new url)
-// router.post('/users...', new user)
-// router.put('/user/:id...', update user)
-// router.get('/user/:id', user prof .. need most pop link)
+router.post('/users', async function (req, res) {
+  let newUser =
+    await
+      User
+        .create(req.query)
+        .then( (d) => d )
+        .catch( (er) => er )
+  res.send(
+    newUser
+  )
+})
+
+// add retrieval of related most visited url and shortened version
+router.get('/users/:id', async function (req, res) {
+  let resp
+  await
+    User
+      .query()
+      .where('id', '=', req.params.id)
+      .then( (d) => resp = d[0] )
+      .catch( (er) => resp = er )
+  if (!!resp) {
+     res.send(resp) // error can be here too
+  } else {
+    res.status(404)
+    res.send({ error: 'That user not found.' })
+  }
+})
+
+router.patch('/users/:id', async function (req, res) {
+  // reminder: verification
+  let resp
+  await
+    User
+      .query()
+      .patchAndFetchById(req.params.id, req.query)
+      .then( (d) => resp = d  ) // can return undefined
+      .catch( (er) => resp = er )
+  if (!!resp) {
+     res.send(resp) // error can be here too
+  } else {
+    res.send({ error: 'Something went wrong.' })
+  }
+})
+
+router.get('/toprequestedurls', async function (req, res) {
+  res.send(
+    await
+      Url
+        .getMostRequested(10, ['id', 'original', 'requests'])
+        .then( (d) => d )
+        .catch( (er) => er )
+  )
+})
+
+router.get('/topvisitedurls', async function (req, res) {
+  res.send(
+    await
+      Url
+        .getMostVisited(10, ['id', 'original', 'visits'])
+        .then( (d) => d )
+        .catch( (er) => er )
+  )
+})
 
 // prefix routes with /api
 app.use('/api', router)
